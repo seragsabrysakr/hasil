0d94e1448dc3c004b2042595bc2d5333b68efe47
seragsabrysakr <srag.sabry@gmail.com>
2022-11-06 10:53:10 +0200

    test

---
+++ lib/haseal-test/config.dart
@@ -0,0 +1,24 @@
+class Config {
+  //WeDeliververFlutter
+  static String key = "ck_835af9281f5258755734b3db398207f193c6c218";
+  static String secret = "cs_e2046dd842e7c5da35488274584934a6a43d326b";
+
+  static String url = "https://hasseal.com/wp-json/wc/v3/";
+  static String customerURL = "customers";
+  static String tokenURL = "https://hasseal.com//wp-json/jwt-auth/v1/token";
+  static String categoriesURL = "products/categories";
+  static String productsURL = "products";
+  static String offersTagID = '264';
+  static String trendingTagID = '265';
+  static String addtoCartURL = "https://hasseal.com/wp-json/cocart/v1/add-item";
+  static String cartURL = "cart";
+  static int userId = 8; //for testing, picked my id
+  static String variableProductsURL = "variations";
+  static String orderURL = "orders";
+  static String currency = "৳";
+}
+
+//max 10 items shown in category lists, top saver, top trending
+
+//token key in auth wp-config
+// define('JWT_AUTH_SECRET_KEY', 'jU&f@]6-RN>[9EM:bL^19@@==-,q1G5UW[xF-JDR0^%bRw1rvW~Gq-+T+eAMJG0@');
